fbc4a6eed1c40d6acdadfb18f354a06970d3276b
omar alsyouf <omaralsoyouf@gmail.com>
2023-08-18 02:58:46 +0100

    update sitemap

--- sitemap-builder.js
+++ sitemap-builder.js
@@ -1,37 +1,37 @@
-const { SitemapStream, streamToPromise } = require("sitemap");
-const { createWriteStream } = require("fs");
-const axios = require("axios");
+const { SitemapStream, streamToPromise } = require('sitemap')
+const { createWriteStream } = require('fs')
+const axios = require('axios')
 
 async function generateSitemap() {
   try {
     const data = await axios.get(
       `https://ali-website-server.onrender.com/api/blogs?fields[0]=id`
-    );
+    )
 
     const links = [
-      { url: "/", changefreq: "weekly", priority: 0.8 },
-      { url: "/blog", changefreq: "weekly", priority: 0.8 },
-      { url: "/about", changefreq: "monthly", priority: 0.7 },
-      { url: "/contact", changefreq: "monthly", priority: 0.7 },
+      { url: '/', changefreq: 'weekly', priority: 0.8 },
+      { url: '/blog', changefreq: 'weekly', priority: 0.8 },
+      { url: '/about', changefreq: 'monthly', priority: 0.7 },
+      { url: '/contact', changefreq: 'monthly', priority: 0.7 },
       ...data.data.data.map((post) => ({
         url: `/blog/${post.id}`,
-        changefreq: "weekly",
+        changefreq: 'weekly',
         priority: 0.9,
       })),
-    ];
+    ]
 
     const sitemapStream = new SitemapStream({
-      hostname: "https://ali-alsyouf.netlify.app/",
-    });
+      hostname: 'https://alialsyoufchemist.com//',
+    })
 
-    sitemapStream.pipe(createWriteStream("./public/sitemap.xml"));
-    links.forEach((link) => sitemapStream.write(link));
-    sitemapStream.end();
+    sitemapStream.pipe(createWriteStream('./public/sitemap.xml'))
+    links.forEach((link) => sitemapStream.write(link))
+    sitemapStream.end()
 
-    console.log("Sitemap generated!");
+    console.log('Sitemap generated!')
   } catch (err) {
-    console.log("Failed to generate a sitemap: ", err);
+    console.log('Failed to generate a sitemap: ', err)
   }
 }
 
-generateSitemap();
+generateSitemap()

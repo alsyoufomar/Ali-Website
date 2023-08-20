fd78a55a7b40c9612d923f1171fbcb4140da8620
omar alsyouf <omaralsoyouf@gmail.com>
2023-08-20 10:53:24 +0100

    update loading message timeout

--- src/pages/loading.js
+++ src/pages/loading.js
@@ -22,7 +22,7 @@ export default function Loading() {
       }, 4000)
     )
 
-    timers.push(setTimeout(() => cycleMessages(), 9000))
+    timers.push(setTimeout(() => cycleMessages(), 10000))
 
     return () => {
       timers.forEach((timer) => clearTimeout(timer))
@@ -35,7 +35,7 @@ export default function Loading() {
     setTimeout(() => {
       setMessageIndex((prevIndex) => (prevIndex + 1) % messages.length)
       setShowLoading(true)
-      setTimeout(cycleMessages, 9000)
+      setTimeout(cycleMessages, 10000)
     }, 500)
   }
 
